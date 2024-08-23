887a41a2be2412faa404b85480bbe1fba1836085
James <jamesg@jamesg.blog>
2024-08-23 16:44:31 +0100

    update development dependencies

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     ],
     packages=find_packages(exclude=("tests",)),
     extras_require={
-        "dev": ["flake8", "black==22.3.0", "isort", "twine", "pytest", "wheel", "flask", "orjson", "tqdm"],
+        "dev": ["flake8", "black==22.3.0", "isort", "twine", "pytest", "wheel", "flask", "orjson", "tqdm", "deepdive"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
