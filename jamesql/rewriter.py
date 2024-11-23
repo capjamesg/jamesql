5f85f3df59969c53fb85dff27813bb74c3f2c889
capjamesg <jamesg@jamesg.blog>
2024-11-23 00:12:10 +0000

    work on AND operator

--- jamesql/rewriter.py
+++ jamesql/rewriter.py
@@ -292,7 +292,7 @@ def simplify_string_query(parser, query, correct_spelling_index=None):
     result = QuerySimplifier()
     result.transform(tree.copy())
 
-    query = simplifier(result.terms)
+    # query = simplifier(result.terms)
     print(query)
     query = " ".join(query).strip()
 
